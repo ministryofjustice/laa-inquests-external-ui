60aa6f75d54a37f37581260a31cae540e5ee548c
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-04-30 11:10:17 +0100

    [IDDS-90] Add back button to declarations page

--- tests/playwright/e2e/apply/declaration.spec.ts
+++ tests/playwright/e2e/apply/declaration.spec.ts
@@ -1,28 +1,47 @@
 import { test, expect } from "../../fixtures/index.js";
-import describe from "@playwright/test"
+import describe from "@playwright/test";
 
-  test("renders declaration subheader and copy text", async ({ page }) => {
-    page.goto("/apply")
-    const declarationHeading = await page.getByRole("heading", { level: 2, name: "Declaration"})
+test("renders declaration subheader, copy text and back button", async ({
+  page,
+}) => {
+  page.goto("/apply");
+  const declarationHeading = await page.getByRole("heading", {
+    level: 2,
+    name: "Declaration",
+  });
 
-    const declarationCopy = page.getByText("By continuing, you agree that:")
+  const declarationCopy = page.getByText("By continuing, you agree that:");
+  const representationDeclaration = await page
+    .getByRole("list")
+    .getByText("Your client has instructed you to represent them");
+  const clientDeclaration = await page
+    .getByRole("list")
+    .getByText(
+      "You'll go through all parts of the application with your client",
+    );
+  const correctDeclaration = await page
+    .getByRole("list")
+    .getByText("You'll provide complete and correct information");
 
-    const representationDeclaration = await page.getByRole("list").getByText("Your client has instructed you to represent them")
-    const clientDeclaration = await page.getByRole("list").getByText("You'll go through all parts of the application with your client")
-    const correctDeclaration = await page.getByRole("list").getByText("You'll provide complete and correct information")
+  const backButton = page.getByRole("link", { name: "Back", exact: true });
 
-    await expect(declarationHeading).toBeVisible()
-    await expect(declarationCopy).toBeVisible()
-    await expect(representationDeclaration).toBeVisible()
-    await expect(clientDeclaration).toBeVisible()
-    await expect(correctDeclaration).toBeVisible()
-  })
+  await expect(declarationHeading).toBeVisible();
+  await expect(declarationCopy).toBeVisible();
+  await expect(representationDeclaration).toBeVisible();
+  await expect(clientDeclaration).toBeVisible();
+  await expect(correctDeclaration).toBeVisible();
 
-  test("renders button to start application journey ", async ({ page }) => {
-      page.goto("/apply")
-      const startButton = page.getByRole("button", { name: "Agree and continue"})
-      await expect(startButton).toBeVisible()
-      await expect(startButton).toHaveAttribute("href", "/apply/basic-client-details")
-      await startButton.click()
+  await expect(backButton).toBeVisible();
+  await expect(backButton).toHaveAttribute("href", "/");
+});
 
-  })
+test("renders button to start application journey ", async ({ page }) => {
+  page.goto("/apply");
+  const startButton = page.getByRole("button", { name: "Agree and continue" });
+  await expect(startButton).toBeVisible();
+  await expect(startButton).toHaveAttribute(
+    "href",
+    "/apply/basic-client-details",
+  );
+  await startButton.click();
+});
