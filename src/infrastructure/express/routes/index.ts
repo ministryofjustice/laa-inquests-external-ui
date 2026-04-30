af3864f5e31102d500da04686168961e4bb079ca
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-04-30 12:30:52 +0100

    [IDDS-90] Add national insurnace details page

--- src/infrastructure/express/routes/index.ts
+++ src/infrastructure/express/routes/index.ts
@@ -23,7 +23,11 @@ router.get("/apply", (req: Request, res: Response): void => {
 router.get(
   "/apply/client-details/name-and-dob",
   (req: Request, res: Response): void => {
-    res.render("apply/client-details/name-and-dob");
+    console.log(res);
+    // eslint-disable-next-line @typescript-eslint/prefer-destructuring -- Temporary disable
+    const { csrfToken } = res.locals;
+    console.log(csrfToken);
+    res.render("apply/client-details/name-and-dob", { csrfToken });
   },
 );
 
@@ -31,6 +35,24 @@ router.post(
   "/apply/client-details/name-and-dob",
   (req: Request, res: Response): void => {
     console.log(req.body);
+    res.redirect("/apply/client-details/nino");
+  },
+);
+
+router.get(
+  "/apply/client-details/nino",
+  (req: Request, res: Response): void => {
+    // eslint-disable-next-line @typescript-eslint/prefer-destructuring -- Temporary disable
+    const { csrfToken } = res.locals;
+    res.render("apply/client-details/nino", { csrfToken });
+  },
+);
+
+router.post(
+  "/apply/client-details/nino",
+  (req: Request, res: Response): void => {
+    console.log(req.body);
+    res.redirect("/apply/client-details/legal-aid-request");
   },
 );
 
