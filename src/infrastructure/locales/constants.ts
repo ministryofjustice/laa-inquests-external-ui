50d19d886079bedaaffb8a9a076ca3809986e2e7
hevdevs <90092645+hevdevs@users.noreply.github.com>
2026-05-05 12:26:11 +0100

    [IDDS-90] add error messages to constants file

--- src/infrastructure/locales/constants.ts
+++ src/infrastructure/locales/constants.ts
@@ -1,3 +1,17 @@
 export const MAX_CHARACTER_LENGTH = 100;
 
 export const EMPTY_ARR_LENGTH = 0;
+
+export const CLIENT_DETAILS_ERROR = {
+  NON_NUMERIC_DATE: "Please enter date of birth in the format expected",
+  MISSING_DOB_INPUT: "Please enter date of birth",
+  FUTURE_DATE: "Date of birth must not be in the future",
+  FIRST_NAME_EXCEEDS_MAX_CHARACTER_LENGTH:
+    "First name(s) cannot exceed 100 characters",
+  LAST_NAME_EXCEEDS_MAX_CHARACTER_LENGTH:
+    "Last name cannot exceed 100 characters",
+  MISSING_FIRST_NAME: "Please enter your client's first name",
+  MISSING_LAST_NAME: "Please enter your client's last name",
+  MISSING_LAST_NAME_AT_BIRTH: "Please enter the client's birth name",
+  INPUT_NOT_SELECTED: "Please select an option",
+};
