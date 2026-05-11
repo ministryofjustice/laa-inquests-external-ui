e8243f642411179911703f9473e9812283722543
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-05-11 09:06:02 +0100

    [IDDS-98] Add functionality for inputs getting filled from session data and redirecting to dob page

--- tests/playwright/e2e/apply/deceased-details/date-of-death.spec.ts
+++ tests/playwright/e2e/apply/deceased-details/date-of-death.spec.ts
@@ -2,7 +2,7 @@ import { DECEASED_DETAILS_ERROR } from "#src/infrastructure/locales/constants.js
 import { test, expect } from "../../../fixtures/index.js";
 import type { Page } from "playwright-core";
 
-test.describe.only("Deceased details - date of death", () => {
+test.describe("Deceased details - date of death", () => {
   test("renders basic details header and back link", async ({ page }) => {
     page.goto("/apply/deceased-details/dod");
     const deceasedDateOfDeathHeading = await page.getByRole("heading", {
@@ -45,74 +45,116 @@ test.describe.only("Deceased details - date of death", () => {
     );
   });
 
-  test("clicking continue with all empty fields shows error", async ({
-    page,
-  }) => {
-    const inputs = {
-      Day: "",
-      Month: "",
-      Year: "",
-    };
+  test.describe("renders error message when", () => {
+    test("clicking continue with all empty fields", async ({ page }) => {
+      const inputs = {
+        Day: "",
+        Month: "",
+        Year: "",
+      };
 
-    await testEmptyInputs(page, inputs);
+      await testEmptyInputs(page, inputs);
+    });
+
+    test("clicking continue with day field empty", async ({ page }) => {
+      const inputs = {
+        Day: "",
+        Month: "1",
+        Year: "1990",
+      };
+
+      await testEmptyInputs(page, inputs);
+    });
+
+    test("clicking continue with month field empty", async ({ page }) => {
+      const inputs = {
+        Day: "1",
+        Month: "",
+        Year: "1990",
+      };
+
+      await testEmptyInputs(page, inputs);
+    });
+
+    test("clicking continue with year field empty", async ({ page }) => {
+      const inputs = {
+        Day: "1",
+        Month: "1",
+        Year: "",
+      };
+
+      await testEmptyInputs(page, inputs);
+    });
+
+    const testEmptyInputs = async (
+      page: Page,
+      inputs: Record<string, string>,
+    ) => {
+      page.goto("/apply/deceased-details/dod");
+
+      const deceasedForm = await page.getByTestId(
+        "deceased-date-of-death-form",
+      );
+
+      for (const [key, value] of Object.entries(inputs)) {
+        const input = deceasedForm.getByLabel(key);
+        input.fill(value);
+      }
+
+      const continueButton = deceasedForm.getByRole("button");
+      await continueButton.click();
+
+      const errorMessage = await deceasedForm.locator(
+        "#deceased-date-of-death-error",
+      );
+      await expect(errorMessage).toBeVisible();
+      await expect(errorMessage).toContainText(
+        DECEASED_DETAILS_ERROR.MISSING_DATE_OF_DEATH_INPUT,
+      );
+    };
   });
 
-  test("clicking continue with day field empty shows error", async ({
-    page,
-  }) => {
-    const inputs = {
-      Day: "",
-      Month: "1",
-      Year: "1990",
-    };
-
-    await testEmptyInputs(page, inputs);
-  });
-
-  test("clicking continue with month field empty shows error", async ({
-    page,
-  }) => {
-    const inputs = {
-      Day: "1",
-      Month: "",
-      Year: "1990",
-    };
-
-    await testEmptyInputs(page, inputs);
-  });
-
-  test("clicking continue with year field empty shows error", async ({
-    page,
-  }) => {
-    const inputs = {
-      Day: "1",
-      Month: "1",
-      Year: "",
-    };
-
-    await testEmptyInputs(page, inputs);
-  });
-
-  const testEmptyInputs = async (
-    page: Page,
-    inputs: Record<string, string>,
-  ) => {
+  test("fills in inputs on error", async ({ page }) => {
     page.goto("/apply/deceased-details/dod");
 
     const deceasedForm = await page.getByTestId("deceased-date-of-death-form");
-
-    for (const [key, value] of Object.entries(inputs)) {
-      const input = deceasedForm.getByLabel(key);
-      input.fill(value);
-    }
-
     const continueButton = deceasedForm.getByRole("button");
+
+    const dayInput = deceasedForm.getByLabel("Day");
+    const monthInput = deceasedForm.getByLabel("Month");
+    const yearInput = deceasedForm.getByLabel("Year");
+
+    await dayInput.fill("1");
+    await monthInput.fill("2");
+    await yearInput.fill("abc");
+
+    await continueButton.click();
+    await page.waitForLoadState("domcontentloaded");
+
+    await expect(dayInput).toHaveValue("1");
+    await expect(monthInput).toHaveValue("2");
+    await expect(yearInput).toHaveValue("abc");
+  });
+
+  test("continuing with valid inputs redirects to deceased dob", async ({
+    page,
+  }) => {
+    page.goto("/apply/deceased-details/dod");
+
+    const deceasedForm = await page.getByTestId("deceased-date-of-death-form");
+    const continueButton = deceasedForm.getByRole("button");
+
+    const dayInput = deceasedForm.getByLabel("Day");
+    const monthInput = deceasedForm.getByLabel("Month");
+    const yearInput = deceasedForm.getByLabel("Year");
+
+    await dayInput.fill("1");
+    await monthInput.fill("1");
+    await yearInput.fill("1990");
+
     await continueButton.click();
 
-    const errorMessage = await deceasedForm.locator("#date-of-death-error");
-    await expect(errorMessage).toBeVisible();
-    await expect(errorMessage).toContainText(
-      DECEASED_DETAILS_ERROR.MISSING_DATE_OF_DEATH_INPUT,
-    );
-  };
+    await page.waitForLoadState("domcontentloaded");
+    await expect(page.url()).toContain("apply/deceased-details/dob");
+  });
 });
