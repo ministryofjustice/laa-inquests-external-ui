9ce9605d67f9167c8b49970bb6f6042ccc2d729d
saraesabbagh <sara.sabbagh@justice.gov.uk>
2026-05-11 15:04:48 +0100

    idds-119-complete check answers summary lists

--- tests/playwright/e2e/apply/check-your-answers.spec.ts
+++ tests/playwright/e2e/apply/check-your-answers.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "../../fixtures/index.js";
 
-test.describe("Apply - check your answers", () => {
+test.describe.only("Apply - check your answers", () => {
   test("renders check your answers page header and back link", async ({ page }) => {
     page.goto("/apply/check-your-answers");
 
@@ -18,20 +18,25 @@ test.describe("Apply - check your answers", () => {
       "/apply/fee-earner",
     );
 
+    
+
+    const pageInsetText = page.getByText("You cannot change the answers on this page once you  continue");
+    await expect(pageInsetText).toBeVisible();
+
+     const continueButton = page.getByRole("button");
+    await expect(continueButton).toHaveText("Continue");
+  });
+
+  test("renders client details summary list", async ({ page }) => {
+
+    page.goto("/apply/check-your-answers");
+
     const caseDetailsHeading = await page.getByRole("heading", {
       level: 2,
       name: "Case details",
     });
 
     await expect(caseDetailsHeading).toBeVisible();
-
-    const pageInsetText = page.getByText("You cannot change the answers on this page once you  continue");
-    await expect(pageInsetText).toBeVisible();
-  });
-
-  test("renders client details summary list", async ({ page }) => {
-
-    page.goto("/apply/check-your-answers");
 
     const clientDetailsSummaryList = page.getByTestId("client-details-summary-list");
     await expect(clientDetailsSummaryList).toBeVisible();
@@ -62,8 +67,105 @@ test.describe("Apply - check your answers", () => {
     await expect(homeAddressRowTitle).toBeVisible();
     await expect(correspondenceAddressRowTitle).toBeVisible();
 
-    const continueButton = page.getByRole("button");
-    await expect(continueButton).toHaveText("Continue");
+   
+
+  });
+  test("renders deceased details summary list", async ({ page }) => {
+
+    page.goto("/apply/check-your-answers");
+
+    const inquestHeading = await page.getByRole("heading", {
+      level: 2,
+      name: "Inquest",
+    });
+
+    await expect(inquestHeading).toBeVisible();
+
+    const deceasedDetailsSummaryList = page.getByTestId("deceased-details-summary-list");
+    await expect(deceasedDetailsSummaryList).toBeVisible();
+
+    const deceasedDetailsTableHeading = await deceasedDetailsSummaryList.getByRole("heading", {
+      level: 2,
+      name: "Deceased details",
+    });
+    await expect(deceasedDetailsTableHeading).toBeVisible();
+
+    const deceasedDetailsChangeLink = deceasedDetailsSummaryList.getByRole("link");
+    await expect(deceasedDetailsChangeLink).toBeVisible();
+
+    await expect(deceasedDetailsChangeLink).toHaveAttribute(
+      "href",
+      "/apply/deceased-details/name",
+    );
+
+    const firstNameRowTitle = deceasedDetailsSummaryList.getByText("First name");
+    const lastNameRowTitle = deceasedDetailsSummaryList.getByText("Last name");
+    const dodTitle = deceasedDetailsSummaryList.getByText("Date of death");
+    const clientRelationshipTitle = deceasedDetailsSummaryList.getByText("Client relationship");
+    const inquestIdTitle = deceasedDetailsSummaryList.getByText("Inquest ID");
+
+    await expect(firstNameRowTitle).toBeVisible();
+    await expect(lastNameRowTitle).toBeVisible();
+    await expect(dodTitle).toBeVisible();
+    await expect(clientRelationshipTitle).toBeVisible();
+    await expect(inquestIdTitle).toBeVisible();
+
+   
+
+  });
+  test("renders interested parties  summary list", async ({ page }) => {
+
+    page.goto("/apply/check-your-answers");
+
+    
+    const interestedPartiesSummaryList = page.getByTestId("interested-parties-summary-list");
+    await expect(interestedPartiesSummaryList).toBeVisible();
+
+    const interestedPartiesTableHeading = await interestedPartiesSummaryList.getByRole("heading", {
+      level: 2,
+      name: "Interested parties",
+    });
+    await expect(interestedPartiesTableHeading).toBeVisible();
+
+    const interestedPartiesChangeLink = interestedPartiesSummaryList.getByRole("link");
+    await expect(interestedPartiesChangeLink).toBeVisible();
+
+    await expect(interestedPartiesChangeLink).toHaveAttribute(
+      "href",
+      "/apply/public-authority",
+    );
+
+    const publicAuthorityRowTitle = interestedPartiesSummaryList.getByText("Public authority");
+    
+    await expect(publicAuthorityRowTitle).toBeVisible();
+
+  });
+
+  test("renders coroner's letter summary list", async ({ page }) => {
+
+    page.goto("/apply/check-your-answers");
+
+    
+    const coronersLetterSummaryList = page.getByTestId("coroners-letter-summary-list");
+    await expect(coronersLetterSummaryList).toBeVisible();
+
+    const coronersLetterTableHeading = await coronersLetterSummaryList.getByRole("heading", {
+      level: 2,
+      name: "Coroner's letter",
+    });
+    await expect(coronersLetterTableHeading).toBeVisible();
+
+    const coronersLetterChangeLink = coronersLetterSummaryList.getByRole("link");
+    await expect(coronersLetterChangeLink).toBeVisible();
+
+    await expect(coronersLetterChangeLink).toHaveAttribute(
+      "href",
+      "/apply/upload",
+    );
+
+    const fileNameRowTitle = coronersLetterSummaryList.getByText("File name");
+    
+    await expect(fileNameRowTitle).toBeVisible();
 
   });
 
