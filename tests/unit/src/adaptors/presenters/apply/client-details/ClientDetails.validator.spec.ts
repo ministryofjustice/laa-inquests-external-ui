823c955b195ea3aa848808509f71a2e475a1ccc5
hevdevs <90092645+hevdevs@users.noreply.github.com>
2026-05-11 11:33:07 +0100

    [IDDS-100] rm accidental test incldued in merge

--- tests/unit/src/adaptors/presenters/apply/client-details/ClientDetails.validator.spec.ts
+++ tests/unit/src/adaptors/presenters/apply/client-details/ClientDetails.validator.spec.ts
@@ -407,33 +407,5 @@ describe("ClientDetailsValidator", () => {
         });
       });
     });
-    describe("validateProceedingInput", () => {
-      it("returns expected error message when no proceeding is selected", () => {
-        const formValidator = new FormValidator();
-        const formBody = {
-          _csrf: "abcdefg",
-        };
-        const errorSummaries = formValidator.validateProceedingInput(formBody);
-        assert.deepEqual(errorSummaries, {
-          noProceedingSelected: {
-            text: PROCEEDING_ERROR.NO_PROCEEDING_SPECIFIED,
-          },
-        });
-      });
-      it("returns expected error message when no proceeding is selected", () => {
-        const formValidator = new FormValidator();
-        const formBody = {
-          _csrf: "abcdefg",
-          "proceeding-option": "Mental Health",
-        };
-        const errorSummaries =
-          formValidator.validateAddAnotherProceeding(formBody);
-        assert.deepEqual(errorSummaries, {
-          noConfirmationSelected: {
-            text: PROCEEDING_ERROR.NO_CONFIRMATION_SPECIFIED,
-          },
-        });
-      });
-    });
   });
 });
