119d4d2eb45e56212c3110afa13bdb3a42a9fb9e
Ashley Perrett <40239096+ashperrett@users.noreply.github.com>
2026-05-12 13:42:30 +0100

    IDDS-119 add client and deceased details to check answers page

--- src/adaptors/presenters/apply/Confirmation/Confirmation.adaptor.ts
+++ src/adaptors/presenters/apply/Confirmation/Confirmation.adaptor.ts
@@ -12,12 +12,16 @@ export class ConfirmationAdaptor {
       client: {
         clientFirstName: req.session.clientFirstName ?? "",
         clientLastName: req.session.clientLastName ?? "",
-        clientDob: req.session.clientDobDay && req.session.clientDobMonth && req.session.clientDobYear ? `${req.session.clientDobDay}/${req.session.clientDobMonth}/${req.session.clientDobYear}` : "" 
+        clientDob: req.session.clientDobDay && req.session.clientDobMonth && req.session.clientDobYear ? `${req.session.clientDobDay}/${req.session.clientDobMonth}/${req.session.clientDobYear}` : "", 
+        clientAddress: `${req.session.clientAddressLine1 ?? ""}${req.session.clientAddressLine2 ?? " "} ${req.session.clientTownOrcity ?? ""} ${req.session.clientCounty ?? ""} ${req.session.clientPostcode ?? ""}`,
+        clientCorrespondenceAddress : `${req.session.clientCorrespondenceAddressLine1 ?? ""}${req.session.clientCorrespondenceAddressLine2 ?? " "} ${req.session.clientCorrespondenceTownOrcity ?? ""} ${req.session.clientCorrespondenceCounty ?? ""} ${req.session.clientCorrespondencePostcode ?? ""}`,
       },
       deceasedDetails: {
         deceasedFirstName: req.session.deceasedFirstName ?? "",
         deceasedLastName: req.session.deceasedLastName ?? "",
-        dateOfDeath: req.session.deceasedDateOfDeathDay && req.session.deceasedDateOfDeathMonth && req.session.deceasedDateOfDeathYear ? `${req.session.deceasedDateOfDeathDay}/${req.session.deceasedDateOfDeathMonth}/${req.session.deceasedDateOfDeathYear}` : ""
+        dateOfDeath: req.session.deceasedDateOfDeathDay && req.session.deceasedDateOfDeathMonth && req.session.deceasedDateOfDeathYear ? `${req.session.deceasedDateOfDeathDay}/${req.session.deceasedDateOfDeathMonth}/${req.session.deceasedDateOfDeathYear}` : "",
+        deceasedClientRelationship: req.session.deceasedClientRelationship ?? "",
+        deceasedCoronerReference: req.session.deceasedCoronerReference ?? ""
       }
     });
   }
--- tests/unit/src/adaptors/presenters/apply/confirmation/Confirmation.adaptor.spec.ts
+++ tests/unit/src/adaptors/presenters/apply/confirmation/Confirmation.adaptor.spec.ts
@@ -28,13 +28,27 @@ describe("Confirmation adaptor", () => {
     requestStub.session.clientDobMonth = "12";
     requestStub.session.clientDobYear = "1990";
 
+    requestStub.session.clientAddressLine1 = "4 Privet Drive";
+    requestStub.session.clientAddressLine2 = "";
+    requestStub.session.clientTownOrcity = "Little Whinging";
+    requestStub.session.clientCounty = "Surrey";
+    requestStub.session.clientPostcode = "B1 123b";
+
+    requestStub.session.clientCorrespondenceAddressLine1 = "1 test rd";
+    requestStub.session.clientCorrespondenceAddressLine2 = "";
+    requestStub.session.clientCorrespondenceTownOrcity = "test city";
+    requestStub.session.clientCorrespondenceCounty = "test county";
+    requestStub.session.clientCorrespondencePostcode = "B1 321b";
+
     requestStub.session.deceasedFirstName = "deceased first name";
     requestStub.session.deceasedLastName = "deceased last name";
 
     requestStub.session.deceasedDateOfDeathDay = "6";
     requestStub.session.deceasedDateOfDeathMonth = "8";
     requestStub.session.deceasedDateOfDeathYear = "2001";
-
+    requestStub.session.deceasedClientRelationship = "brother";
+    requestStub.session.deceasedCoronerReference = "12345678910";
+    
 
     confirmationAdaptor.renderCheckYourAnswers(requestStub, responseStub);
     assert.equal(responseStub.render.callCount, 1);
@@ -45,12 +59,16 @@ describe("Confirmation adaptor", () => {
         client: {
           clientFirstName:"test name",
           clientLastName:"last name",
-          clientDob: "1/12/1990"
+          clientDob: "1/12/1990",
+          clientAddress: "4 Privet Drive Little Whinging Surrey B1 123b",
+          clientCorrespondenceAddress: "1 test rd test city test county B1 321b"
         },
         deceasedDetails: {
           deceasedFirstName:"deceased first name",
           deceasedLastName:"deceased last name",
-          dateOfDeath: "6/8/2001"
+          dateOfDeath: "6/8/2001",
+          deceasedClientRelationship: "brother",
+          deceasedCoronerReference: "12345678910"
         }
       });
   });
