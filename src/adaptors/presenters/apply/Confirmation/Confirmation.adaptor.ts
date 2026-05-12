058125ad751729385e2ac140b54e40720587ef1c
Ashley Perrett <40239096+ashperrett@users.noreply.github.com>
2026-05-12 15:35:40 +0100

    IDDS-119 fix linting issues

--- src/adaptors/presenters/apply/Confirmation/Confirmation.adaptor.ts
+++ src/adaptors/presenters/apply/Confirmation/Confirmation.adaptor.ts
@@ -11,27 +11,41 @@ export class ConfirmationAdaptor {
     const {
       locals: { csrfToken },
     } = res;
-    console.log(req.session.selectedProceedings);
 
     const publicAuthorities = this.formatter.formatIntoTableRows(req.session.selectedPublicAuthorities ?? []);
+    
+    const clientDob = this.createDateString(req.session.clientDobDay as string, req.session.clientDobMonth as string, req.session.clientDobYear as string);
+    const dateOfDeath = this.createDateString(req.session.deceasedDateOfDeathDay as string, req.session.deceasedDateOfDeathMonth as string, req.session.deceasedDateOfDeathYear as string);
+
+    const clientAddress = this.createAddressString(req.session.clientAddressLine1 as string, req.session.clientAddressLine2 as string, req.session.clientTownOrcity as string, req.session.clientCounty as string, req.session.clientPostcode as string);
+    const clientCorrespondenceAddress = this.createAddressString(req.session.clientCorrespondenceAddressLine1 as string, req.session.clientCorrespondenceAddressLine2 as string, req.session.clientCorrespondenceTownOrcity as string, req.session.clientCorrespondenceCounty as string, req.session.clientCorrespondencePostcode as string);
 
     res.render("apply/check-your-answers", {
       csrfToken,
       client: {
         clientFirstName: req.session.clientFirstName ?? "",
         clientLastName: req.session.clientLastName ?? "",
-        clientDob: req.session.clientDobDay && req.session.clientDobMonth && req.session.clientDobYear ? `${req.session.clientDobDay}/${req.session.clientDobMonth}/${req.session.clientDobYear}` : "", 
-        clientAddress: `${req.session.clientAddressLine1 ?? ""}${req.session.clientAddressLine2 ?? " "} ${req.session.clientTownOrcity ?? ""} ${req.session.clientCounty ?? ""} ${req.session.clientPostcode ?? ""}`,
-        clientCorrespondenceAddress : `${req.session.clientCorrespondenceAddressLine1 ?? ""}${req.session.clientCorrespondenceAddressLine2 ?? " "} ${req.session.clientCorrespondenceTownOrcity ?? ""} ${req.session.clientCorrespondenceCounty ?? ""} ${req.session.clientCorrespondencePostcode ?? ""}`,
+        clientDob: clientDob,
+        clientAddress: clientAddress,
+        clientCorrespondenceAddress: clientCorrespondenceAddress
       },
       deceasedDetails: {
         deceasedFirstName: req.session.deceasedFirstName ?? "",
         deceasedLastName: req.session.deceasedLastName ?? "",
-        dateOfDeath: req.session.deceasedDateOfDeathDay && req.session.deceasedDateOfDeathMonth && req.session.deceasedDateOfDeathYear ? `${req.session.deceasedDateOfDeathDay}/${req.session.deceasedDateOfDeathMonth}/${req.session.deceasedDateOfDeathYear}` : "",
+        dateOfDeath: dateOfDeath,
         deceasedClientRelationship: req.session.deceasedClientRelationship ?? "",
         deceasedCoronerReference: req.session.deceasedCoronerReference ?? ""
       },
       publicAuthorities
     });
   }
+
+  createDateString(day?: string, month?: string, year?: string): string{
+    return (typeof day === 'string' && typeof month === 'string' && typeof year === 'string') ? `${day}/${month}/${year}` : "";
+  }
+
+  createAddressString(addressLine1?: string, addressLine2?: string, townOrcity?: string, county?: string, postcode?: string): string{
+    return `${addressLine1 ?? ""}${addressLine2 ?? " "} ${townOrcity ?? ""} ${county ?? ""} ${postcode ?? ""}`;
+  }
+
 }
