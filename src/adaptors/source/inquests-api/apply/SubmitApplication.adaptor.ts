332b5012aa4fc6620469e200886c11cfe12e6298
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-05-15 14:55:31 +0100

    [IDDS-92] Cleanup adaptor

--- src/adaptors/source/inquests-api/apply/SubmitApplication.adaptor.ts
+++ src/adaptors/source/inquests-api/apply/SubmitApplication.adaptor.ts
@@ -5,6 +5,7 @@ import type {
 } from "#src/ports/source/inquests-api/SubmitApplication.port.js";
 
 export class SubmitApplicationDomain implements ApplySubmitPort {
+  // eslint-disable-next-line @typescript-eslint/require-await -- Temp disable
   async submitApplication(
     _body: SubmitApplicationRequest,
   ): Promise<SubmitApplicationResponse> {
