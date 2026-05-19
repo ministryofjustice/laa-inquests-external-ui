40f7bda1e001ff39047207f7ea0baa2e4cd4fae3
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-05-19 15:25:06 +0100

    [IDDS-92] Combine submit and confirmation adaptors into one

--- src/adaptors/presenters/apply/Confirmation/Confirmation.adaptor.ts
+++ src/adaptors/presenters/apply/Confirmation/Confirmation.adaptor.ts
@@ -1,10 +1,22 @@
 import type { Request, Response } from "express";
 import type { Formatter } from "#src/utils/Formatter.js";
+import type { ApplySubmitPort } from "#src/ports/source/inquests-api/SubmitApplication.port.js";
+import {
+  SubmitApplicationRequestSchema,
+  SubmitApplicationResponseSchema,
+} from "#src/adaptors/source/inquests-api/apply/SubmitApplication/models/SubmitApplication.schema.js";
+import type { Proceeding } from "#src/infrastructure/express/session/index.types.js";
+import { formatDateDDMMYYYY } from "#src/utils/dateFormatter.js";
+import type { SubmitApplicationRequest } from "#src/adaptors/source/inquests-api/apply/SubmitApplication/models/SubmitApplication.types.js";
+import { HTTP_CREATED } from "#src/infrastructure/locales/constants.js";
 
 export class ConfirmationAdaptor {
   formatter: Formatter;
-  constructor(formatter: Formatter) {
+  applySubmitPort: ApplySubmitPort;
+
+  constructor(formatter: Formatter, applySubmitPort: ApplySubmitPort) {
     this.formatter = formatter;
+    this.applySubmitPort = applySubmitPort;
   }
 
   renderCheckYourAnswers(req: Request, res: Response): void {
@@ -68,6 +80,98 @@ export class ConfirmationAdaptor {
       },
       publicAuthorities,
     });
+  }
+
+  renderClientDeclarationForm(req: Request, res: Response): void {
+    const {
+      locals: { csrfToken },
+    } = res;
+
+    res.render("apply/submit/client-declaration", {
+      csrfToken,
+      clientDetails: {
+        firstName: req.session.clientFirstName ?? "",
+        lastName: req.session.clientLastName ?? "",
+      },
+    });
+  }
+
+  async processClientDeclarationForm(
+    req: Request,
+    res: Response,
+  ): Promise<void> {
+    const submitBody = this.#generateSubmitBody(req);
+    const { session } = req;
+    const responseRaw =
+      await this.applySubmitPort.submitApplication(submitBody);
+    const { statusCode, laaReference } =
+      SubmitApplicationResponseSchema.parse(responseRaw);
+
+    if (statusCode === HTTP_CREATED) {
+      session.applicationReferenceNumber = laaReference.toString();
+      res.redirect("/apply/confirmation/success");
+    }
+  }
+
+  #generateSubmitBody(req: Request): SubmitApplicationRequest {
+    const { session } = req;
+    const selectedProceedings = req.session.selectedProceedings ?? [];
+    const publicBodies =
+      req.session.selectedPublicAuthorities?.map((body) => ({
+        publicBodyId: body.publicAuthorityDescription,
+      })) ?? [];
+    const { clientLastNameAtBirth, clientNino } = session;
+
+    const client: SubmitApplicationRequest["client"] = {
+      clientFirstName: req.session.clientFirstName as string,
+      clientLastName: req.session.clientLastName as string,
+      dateOfBirth: formatDateDDMMYYYY(
+        req.session.clientDobYear,
+        req.session.clientDobMonth,
+        req.session.clientDobDay,
+      ),
+    };
+
+    if (typeof clientLastNameAtBirth === "string") {
+      client.clientLastNameAtBirth = clientLastNameAtBirth;
+    }
+
+    if (typeof clientNino === "string") {
+      client.nationalInsuranceNumber = clientNino;
+    }
+
+    // Build the payload
+    const submitBodyRaw = {
+      client,
+      deceased: {
+        deceasedFirstName: req.session.deceasedFirstName as string,
+        deceasedLastName: req.session.deceasedLastName as string,
+        deceasedDateOfBirth: formatDateDDMMYYYY(
+          req.session.deceasedDateOfBirthYear,
+          req.session.deceasedDateOfBirthMonth,
+          req.session.deceasedDateOfBirthDay,
+        ),
+        deceasedDateOfDeath: formatDateDDMMYYYY(
+          req.session.deceasedDateOfDeathYear,
+          req.session.deceasedDateOfDeathMonth,
+          req.session.deceasedDateOfDeathDay,
+        ),
+        coronersReference: (req.session.deceasedCoronerReference ??
+          "") as string,
+        furtherInformation: (req.session.deceasedFurtherInformation ??
+          "") as string,
+        clientRelationshipToDeceased: (req.session.deceasedClientRelationship ??
+          "") as string,
+      },
+      proceedings: selectedProceedings.map((proceeding: Proceeding) => ({
+        proceedingId: proceeding.proceedingId,
+      })),
+      publicBodies,
+    };
+
+    // Validate and parse with Zod
+    const submitBody = SubmitApplicationRequestSchema.parse(submitBodyRaw);
+    return submitBody;
   }
 
   renderConfirmSuccess(req: Request, res: Response): void {
