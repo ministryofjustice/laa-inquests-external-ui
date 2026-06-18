0a26d19f4b204dfd2dc4dd4d2601d98ae9ddc95c
Ben Madley <ben.madley@madetech.com>
2026-06-18 10:40:05 +0100

    [IDDS-0] Remove unused date formatter.

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -1,17 +1,3 @@
-export function formatDate(dateString: string): string {
-  const date = new Date(dateString);
-
-  if (isNaN(date.getTime())) {
-    return dateString;
-  }
-
-  const day = date.getDate();
-  const month = date.toLocaleString("en-GB", { month: "short" });
-  const year = date.getFullYear();
-
-  return `${day} ${month} ${year}`;
-}
-
 const DATE_PADDING = 2;
 
 export function formatDateDDMMYYYY(
