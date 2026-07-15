225a5d182fccc86678e98a25d462be81477368e1
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-07-15 16:42:28 +0100

    [IDDS-0] Remove axios middleware

--- src/infrastructure/express/index.types.ts
+++ src/infrastructure/express/index.types.ts
@@ -1,11 +1,9 @@
 import type { Session, SessionData } from "express-session";
-import type { AxiosInstanceWrapper } from "#src/infrastructure/express/middleware/axios/index.types.js";
 import type { ExpressLocaleLoader } from "#src/infrastructure/express/middleware/nunjucks/i18nLoader.js";
 
 declare global {
   namespace Express {
     interface Request {
-      axiosMiddleware: AxiosInstanceWrapper;
       locale: ExpressLocaleLoader;
       locals: {
         csrfToken?: string;
