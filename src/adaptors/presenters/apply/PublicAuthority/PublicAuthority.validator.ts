bd49b6bf5bfb5299631a82156d8aca834bd624f5
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-07-28 15:20:38 +0100

    [IDDS-435] Remove validator functionality around adding multiple public authorities

--- src/adaptors/presenters/apply/PublicAuthority/PublicAuthority.validator.ts
+++ src/adaptors/presenters/apply/PublicAuthority/PublicAuthority.validator.ts
@@ -1,27 +1,15 @@
 import { FormValidator } from "#src/utils/FormValidator.js";
-import { EMPTY_ARR_LENGTH } from "#src/infrastructure/locales/constants.js";
 
 export interface PublicAuthorityError {
   noPublicAuthoritySelected?: { text: string };
-  noConfirmationSelected?: { text: string };
-  noPublicAuthoritiesInList?: { text: string };
 }
 
 export interface PublicAuthorityFormData {
   publicAuthorityOption?: string;
-  "add-another-public-authority"?: string;
-}
-
-export interface RemovePublicAuthorityFormData {
-  publicAuthorityId: string;
-  "remove-public-authority": string;
 }
 
 export const PUBLIC_AUTHORITY_ERROR = {
   NO_SELECTION: "Please select a public authority",
-  NO_CONFIRMATION: "Please select either yes or no to continue.",
-  NO_PUBLIC_AUTHORITIES_IN_LIST:
-    "A case must have a minimum of 1 interested party",
 };
 
 export class PublicAuthorityValidator extends FormValidator {
@@ -35,37 +23,6 @@ export class PublicAuthorityValidator extends FormValidator {
     if (typeof publicAuthorityOption !== "string") {
       errorSummaries.noPublicAuthoritySelected = {
         text: PUBLIC_AUTHORITY_ERROR.NO_SELECTION,
-      };
-    }
-
-    return errorSummaries;
-  }
-
-  validateAddAnotherPublicAuthority(
-    formBody: Partial<PublicAuthorityFormData>,
-  ): Partial<PublicAuthorityError> {
-    const errorSummaries: Partial<PublicAuthorityError> = {};
-
-    const { "add-another-public-authority": isAddingAnotherPublicAuthority } =
-      formBody;
-
-    if (typeof isAddingAnotherPublicAuthority !== "string") {
-      errorSummaries.noConfirmationSelected = {
-        text: PUBLIC_AUTHORITY_ERROR.NO_CONFIRMATION,
-      };
-    }
-
-    return errorSummaries;
-  }
-
-  validatePublicAuthorityList(
-    selectedPublicAuthorities: unknown[],
-  ): Partial<PublicAuthorityError> {
-    const errorSummaries: Partial<PublicAuthorityError> = {};
-
-    if (selectedPublicAuthorities.length === EMPTY_ARR_LENGTH) {
-      errorSummaries.noPublicAuthoritiesInList = {
-        text: PUBLIC_AUTHORITY_ERROR.NO_PUBLIC_AUTHORITIES_IN_LIST,
       };
     }
 
--- tests/unit/src/adaptors/presenters/apply/public-authority/PublicAuthority.validator.spec.ts
+++ tests/unit/src/adaptors/presenters/apply/public-authority/PublicAuthority.validator.spec.ts
@@ -1,7 +1,6 @@
 import {
   PublicAuthorityValidator,
   PUBLIC_AUTHORITY_ERROR,
-  PublicAuthorityFormData,
 } from "#src/adaptors/presenters/apply/PublicAuthority/PublicAuthority.validator.js";
 import { assert } from "chai";
 
@@ -20,25 +19,6 @@ describe("PublicAuthorityValidator", () => {
       assert.deepEqual(errorSummaries, {
         noPublicAuthoritySelected: {
           text: PUBLIC_AUTHORITY_ERROR.NO_SELECTION,
-        },
-      });
-    });
-  });
-
-  describe("validateAddAnotherPublicAuthority", () => {
-    it("returns expected error message when no confirmation option is selected", () => {
-      const formValidator = new PublicAuthorityValidator();
-
-      const formBody: Partial<Record<string, string>> = {
-        _csrf: "abcdefg",
-      };
-
-      const errorSummaries =
-        formValidator.validateAddAnotherPublicAuthority(formBody);
-
-      assert.deepEqual(errorSummaries, {
-        noConfirmationSelected: {
-          text: PUBLIC_AUTHORITY_ERROR.NO_CONFIRMATION,
         },
       });
     });
