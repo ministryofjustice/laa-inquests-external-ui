a634d6d250f01b706bc78334c2fe1a7fc6a8434b
Tommy Mapp <Tommy.Mapp@justice.gov.uk>
2026-07-31 12:04:36 +0100

    [IDDS-435] Remove unneeded state file

--- src/use-cases/apply/publicAuthority/models/publicAuthoritySessionState.types.ts
+++
@@ -1,5 +0,0 @@
-import type { PublicAuthority } from "#src/infrastructure/express/session/index.types.js";
-
-export interface PublicAuthoritySessionState {
-  selectedPublicAuthorities?: PublicAuthority[];
-}
