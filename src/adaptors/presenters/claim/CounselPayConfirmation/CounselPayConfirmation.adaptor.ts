0b0085aec7a8bf0cceccb3cefc1066c50d5a8d25
Ashley Perrett <40239096+ashperrett@users.noreply.github.com>
2026-08-26 10:17:36 +0100

    Revert "IDDS-520 link counsel details to end date page in claim journey"
    This reverts commit 09d1e9aedcfdb49150bf14bf33c607171c6e7349.

--- src/adaptors/presenters/claim/CounselPayConfirmation/CounselPayConfirmation.adaptor.ts
+++ src/adaptors/presenters/claim/CounselPayConfirmation/CounselPayConfirmation.adaptor.ts
@@ -7,7 +7,6 @@ import type {
 } from "./CounselPayConfirmation.validator.js";
 import {
   CLAIM_CHECK_YOUR_ANSWERS_PATH,
-  CLAIM_END_DATE_PATH,
   EMPTY_ARR_LENGTH,
 } from "#src/infrastructure/locales/constants.js";
 import { ClaimNavigationHelper } from "#src/adaptors/presenters/claim/ClaimNavigation.helper.js";
@@ -59,18 +58,12 @@ export class CounselPayConfirmationAdaptor {
         errorSummaries,
       });
     } else {
-      const returningToCheckYourAnswers =
-        this.navigationHelper.isReturningToCheckYourAnswers(req);
       req.session.claim = {
         ...req.session.claim,
         counselBillsPaid: true,
       };
       this.navigationHelper.clearReturnToCheckYourAnswersFlag(req);
-      res.redirect(
-        returningToCheckYourAnswers
-          ? CLAIM_CHECK_YOUR_ANSWERS_PATH
-          : CLAIM_END_DATE_PATH,
-      );
+      res.redirect(CLAIM_CHECK_YOUR_ANSWERS_PATH);
     }
   }
 }
--- src/infrastructure/locales/constants.ts
+++ src/infrastructure/locales/constants.ts
@@ -27,7 +27,6 @@ export const COUNSEL_NUMBER_ZERO = "0";
 
 export const CHECK_YOUR_ANSWERS_ORIGIN = "check-your-answers";
 export const CLAIM_CHECK_YOUR_ANSWERS_PATH = "/claim/check-your-answers";
-export const CLAIM_END_DATE_PATH = "/claim/end-date";
 
 export const COUNSEL_NUMBER_OPTIONS = [
   { value: "0", text: "0" },
